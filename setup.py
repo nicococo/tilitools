b1f994081851631b6f17d4ae99695c6cfea8bc07
nicococo <sinnl0s>
2018-11-10 07:28:48 +0100

    Changed travis and install tools to support python 3 only

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-description = "Tilitools is basically a collection of (non-mainstream) machine learning model and tools with a special focus on" \
+description = "Tilitools is  a collection of (non-mainstream) machine learning model and tools with a special focus on" \
               "anomaly detection, one-class learning, and structured data. Furthermore, we emphasize" \
               "simplicity and ease-of-use *not* runtime performance (although we put some effort into optimization)." \
               "Descriptive examples can be found in the notebooks/ and scripts/ sub-directories. "
@@ -14,8 +14,16 @@ config = {
     'url': 'https://github.com/nicococo/tilitools',
     'author': 'Nico Goernitz',
     'author_email': 'nico.goernitz@tu-berlin.de',
-    'version': '2017.03',
-    'install_requires': ['nose','scikit-learn','numpy', 'scipy', 'matplotlib', 'cvxopt', 'numba'],
+    'version': '2018.11',
+    'install_requires': ['nose',
+                         'scikit-learn',
+                         'numpy',
+                         'scipy',
+                         'matplotlib',
+                         'cvxopt',
+                         'numba',
+                         'pytorch',
+                         'scikit-image'],
     'packages': ['tilitools'],
     'package_dir' : {'tilitools': 'tilitools'},
     'classifiers':['Intended Audience :: Science/Research',
@@ -24,8 +32,8 @@ config = {
                    'Operating System :: POSIX',
                    'Operating System :: Unix',
                    'Operating System :: MacOS',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7']
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6']
 }
 
 setup(**config)
